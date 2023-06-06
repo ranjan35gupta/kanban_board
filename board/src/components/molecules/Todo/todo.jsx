269056f9a38a572c70336fbd8b93a6bd83ee0e06
Ranjan35gupta <Ranjan35gupta@gmail.com>
2023-06-06 18:05:39 +0530

    basic changes on data adding

--- board/src/components/molecules/Todo/todo.jsx
+++ board/src/components/molecules/Todo/todo.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import styles from "./todo.module.css";
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import List from "../../atoms/List/list";
 import { useDispatch,useSelector } from "react-redux";
 import { addTitle,addTodo } from "../../../Redux/Reduxslice/todoSlice";
@@ -21,7 +22,7 @@ const Todo = () => {
 
 
   const handleCard = () => {
-    dispatch(addTodo({id:Math.random()*1000,title:<input  defaultValue={title}   />,inputtag:<input/>}))
+    dispatch(addTodo({id:Math.random()*1000,title:<input  defaultValue={title}/>,inputtag:<List/>}))
 
     setShowList(true);
     setIsClicked(true)
@@ -47,17 +48,19 @@ const Todo = () => {
       )}
 
       {showList ? (
-        <>
+        <div className={styles.todo_container_list}>
           {todo.map((item) => {
             return (
               <>
-                <div>{item.title}
-                {item.inputtag}
-                <button>add cart</button></div>
+                <div >
+                  <div>{item.title}
+                 <MoreHorizIcon/></div>
+                 {item.inputtag}
+                </div>
               </>
             );
           })}
-        </>
+        </div>
       ) : (
         <></>
       )}
